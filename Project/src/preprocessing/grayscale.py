efa09ccfaf6c07ba661f390710bfebaea911e4a8
Tai Huy <23110023@student.hcmute.edu.vn>
2025-12-09 22:45:50 +0700

    sửa file

--- Project/src/preprocessing/grayscale.py
+++
@@ -1,9 +0,0 @@
-﻿"# Placeholder: convert image to grayscale\n"
-import cv2
-
-
-def apply_grayscale(image):
-
-    if len(image.shape) == 3:
-        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return image
---
+++ Project/src/preprocessing/morphology/grayscale.py
@@ -0,0 +1,9 @@
+﻿"# Placeholder: convert image to grayscale\n"
+import cv2
+
+
+def apply_grayscale(image):
+
+    if len(image.shape) == 3:
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
